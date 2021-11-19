23d2cb4f3f1668fa3787c9e1f3f60021f4482f5b
Liliya <liliyavoloshine@gmail.com>
2021-11-19 21:30:23 +0400

    feat: add orbit controls

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three'
-
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 // contaner
 const scene = new THREE.Scene()
 
@@ -22,14 +22,32 @@ camera.position.setZ(30)
 renderer.render(scene, camera)
 
 const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 )
-const material = new THREE.MeshNormalMaterial( { color: 0xffff00 } )
+const material = new THREE.MeshStandardMaterial( { color: 0xff6347 } )
+// const material = new THREE.MeshNormalMaterial( { color: 0xffff00 } )
 const torusKnot = new THREE.Mesh( geometry, material )
 scene.add(torusKnot)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(30,30,30)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+ambientLight.position.set(30,30,30)
+
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
 
 function animate() {
   requestAnimationFrame(animate)
   torusKnot.rotation.x += 0.01
-  torusKnot.rotation.y += 0.01
+  torusKnot.rotation.y += 0.001
+  torusKnot.rotation.z += 0.01
+
+  controls.update()
   renderer.render(scene, camera)
 }
 
