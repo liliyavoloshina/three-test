2bff96bf095c1b138b66e9183a048a8aefa6bc0d
Liliya <liliyavoloshine@gmail.com>
2021-11-20 08:04:19 +0400

    feat: add bg color

--- main.js
+++ main.js
@@ -22,24 +22,37 @@ camera.position.setZ(30)
 renderer.render(scene, camera)
 
 const geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16)
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 })
-// const material = new THREE.MeshNormalMaterial( { color: 0xffff00 } )
+const material = new THREE.MeshNormalMaterial( { color: 0xffff00 } )
 const torusKnot = new THREE.Mesh(geometry, material)
 scene.add(torusKnot)
 
+// light 
 const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(30, 30, 30)
+pointLight.position.set(5, 5, 5)
 
 const ambientLight = new THREE.AmbientLight(0xffffff)
-ambientLight.position.set(30, 30, 30)
 
 scene.add(pointLight, ambientLight)
+
+// light helpers 
 
 const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50)
 scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+
+const animate = () => {
+  requestAnimationFrame(animate)
+  torusKnot.rotation.x += 0.01
+  torusKnot.rotation.y += 0.001
+  torusKnot.rotation.z += 0.01
+
+  controls.update()
+  renderer.render(scene, camera)
+}
+
+animate()
 
 const addStar = () => {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24)
@@ -54,14 +67,5 @@ const addStar = () => {
 
 Array(200).fill().forEach(addStar)
 
-const animate = () => {
-  requestAnimationFrame(animate)
-  torusKnot.rotation.x += 0.01
-  torusKnot.rotation.y += 0.001
-  torusKnot.rotation.z += 0.01
-
-  controls.update()
-  renderer.render(scene, camera)
-}
-
-animate()
+const bgColor = new THREE.Color(0x800080);
+scene.background = bgColor
